f1ac70c4bf0544e1c4cef0df4b93fc06af3effb3
Young Zachariah <yongdit3@gmail.com>
2025-04-16 10:21:20 +0300

    handling state management

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ const counter=document.getElementById('counter');
 const searchInput=document.querySelector('.search-input');
 const filterButtons=document.querySelectorAll('.filter-btn');
 
+//This section demonstrates how to handles state management in Javascript
+let resources =JSON.parse(localStorage.getItem('resources'));
+let currentFilter="All";
+let searchTerm='';
